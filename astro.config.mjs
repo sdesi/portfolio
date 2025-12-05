65cfb345dccb2ca7adebdaf7bcf7c2f36afcbd30
sdesi <whitewyksa@ya.ru>
2025-12-05 22:55:58 +0300

    7 commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  // Прод-адрес сайта
   site: "https://sdesi.github.io/portfolio",
+  // Репозиторий называется "portfolio", поэтому base именно такой:
   base: "/portfolio/",
 });
